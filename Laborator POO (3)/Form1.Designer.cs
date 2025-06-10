d6e70894440ab34398440e47193d96c62fc1edcd
Andrei Popovici <andrei.popovici05@gmail.com>
2025-06-10 09:16:05 +0300

    Add project files.

---
+++ Laborator POO (3)/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace Laborator_POO__3_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.emisie = new System.Windows.Forms.TextBox();
+            this.cmc = new System.Windows.Forms.TextBox();
+            this.an = new System.Windows.Forms.TextBox();
+            this.calculeaza = new System.Windows.Forms.Button();
+            this.rezultat = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.norma = new System.Windows.Forms.ComboBox();
+            this.darkmode = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.butonLight = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(269, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(458, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CALCUL TAXĂ ANUALĂ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Emisie CO2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Capacitate cilindrică:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 315);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Normă de poluare:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(100, 376);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "An fabricare:";
+            // 
+            // emisie
+            // 
+            this.emisie.Location = new System.Drawing.Point(211, 183);
+            this.emisie.Name = "emisie";
+            this.emisie.Size = new System.Drawing.Size(173, 27);
+            this.emisie.TabIndex = 5;
+            // 
+            // cmc
+            // 
+            this.cmc.Location = new System.Drawing.Point(211, 247);
+            this.cmc.Name = "cmc";
+            this.cmc.Size = new System.Drawing.Size(173, 27);
+            this.cmc.TabIndex = 6;
+            // 
+            // an
+            // 
+            this.an.Location = new System.Drawing.Point(211, 373);
+            this.an.Name = "an";
+            this.an.Size = new System.Drawing.Size(173, 27);
+            this.an.TabIndex = 8;
+            // 
+            // calculeaza
+            // 
+            this.calculeaza.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.calculeaza.Location = new System.Drawing.Point(376, 474);
+            this.calculeaza.Name = "calculeaza";
+            this.calculeaza.Size = new System.Drawing.Size(254, 50);
+            this.calculeaza.TabIndex = 9;
+            this.calculeaza.Text = "Calculează";
+            this.calculeaza.UseVisualStyleBackColor = true;
+            this.calculeaza.Click += new System.EventHandler(this.calculeaza_Click);
+            // 
+            // rezultat
+            // 
+            this.rezultat.Font = new System.Drawing.Font("Courier New", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.rezultat.Location = new System.Drawing.Point(642, 234);
+            this.rezultat.Multiline = true;
+            this.rezultat.Name = "rezultat";
+            this.rezultat.ReadOnly = true;
+            this.rezultat.Size = new System.Drawing.Size(278, 66);
+            this.rezultat.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(269, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(458, 22);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "IMPORTANT! Vă rugăm să completați toate câmpurile prezente!";
+            // 
+            // norma
+            // 
+            this.norma.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.norma.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.norma.FormattingEnabled = true;
+            this.norma.Items.AddRange(new object[] {
+            "Electric",
+            "Hibrid",
+            "Euro 6",
+            "Euro 5",
+            "Euro 4",
+            "Euro 3",
+            "Euro 2",
+            "Euro 1",
+            "Non-euro"});
+            this.norma.Location = new System.Drawing.Point(211, 315);
+            this.norma.Name = "norma";
+            this.norma.Size = new System.Drawing.Size(173, 28);
+            this.norma.TabIndex = 12;
+            // 
+            // darkmode
+            // 
+            this.darkmode.Location = new System.Drawing.Point(824, 24);
+            this.darkmode.Name = "darkmode";
+            this.darkmode.Size = new System.Drawing.Size(150, 32);
+            this.darkmode.TabIndex = 13;
+            this.darkmode.Text = "Mod întunecat";
+            this.darkmode.UseVisualStyleBackColor = true;
+            this.darkmode.Click += new System.EventHandler(this.darkmode_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(667, 194);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(224, 28);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Rezultatul va fi afișat aici";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // butonLight
+            // 
+            this.butonLight.Location = new System.Drawing.Point(12, 24);
+            this.butonLight.Name = "butonLight";
+            this.butonLight.Size = new System.Drawing.Size(150, 32);
+            this.butonLight.TabIndex = 15;
+            this.butonLight.Text = "Mod luminos";
+            this.butonLight.UseVisualStyleBackColor = true;
+            this.butonLight.Click += new System.EventHandler(this.butonLight_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 541);
+            this.Controls.Add(this.butonLight);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.darkmode);
+            this.Controls.Add(this.norma);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.rezultat);
+            this.Controls.Add(this.calculeaza);
+            this.Controls.Add(this.an);
+            this.Controls.Add(this.cmc);
+            this.Controls.Add(this.emisie);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Taxă anuală v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+//            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox emisie;
+        private System.Windows.Forms.TextBox cmc;
+        private System.Windows.Forms.TextBox an;
+        private System.Windows.Forms.Button calculeaza;
+        private System.Windows.Forms.TextBox rezultat;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox norma;
+        private System.Windows.Forms.Button darkmode;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button butonLight;
+    }
+}
+
---
+++ Laborator POO (3)/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laborator_POO__3_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculeaza_Click(object sender, EventArgs e)
+        {
+            int em, cmc_nr, an_nr;
+            string norma_string;
+            double suma_plata = 0.0;
+            double A, B, C, D, E;
+            try
+            {
+                em = Convert.ToInt32(emisie.Text);
+                try
+                {
+                    cmc_nr = Convert.ToInt32(cmc.Text);
+                    try
+                    {
+                        norma_string = norma.SelectedItem.ToString();
+                        try
+                        {
+                            an_nr = Convert.ToInt32(an.Text);
+                            rezultat.Text = "";
+                            //CALCULARE TAXA NORMA
+                            if (norma_string == "Hibrid" || norma_string == "Electric" || norma_string == "Euro 6")
+                            {
+                                B = 0;
+                            }
+                            else if (norma_string == "Euro 3" || norma_string == "Euro 4" || norma_string == "Euro 5")
+                            {
+                                if (em <= 120) B = 0;
+                                else if (em >= 121 && em <= 210) B = em;
+                                else if (em >= 211 && em <= 270) B = 4 * em;
+                                else B = 8 * em;
+                            }
+                            else B = 0;
+
+                            //CALCULARE TAXA EMISII
+                            if (norma_string == "Hibrid" || norma_string == "Electric" || norma_string == "Euro 6")
+                            {
+                                D = 0;
+                            }
+                            else if (norma_string == "Euro 5")
+                            {
+                                if (cmc_nr <= 2000) D = cmc_nr * 1.3;
+                                else D = cmc_nr * 0.39;
+                            }
+                            else if (norma_string == "Euro 4")
+                            {
+                                if (cmc_nr <= 2000) D = cmc_nr * 0.13;
+                                else D = cmc_nr * 3.0;
+                            }
+                            else
+                            {
+                                if (cmc_nr <= 2000) D = cmc_nr * 9.0;
+                                else D = cmc_nr * 16.0;
+                            }
+
+                            //COTA DE REDUCERE A TAXEI
+                            int diferenta_an = 2021 - an_nr;
+                            if (diferenta_an == 0)
+                            {
+                                E = 0;
+                            }
+                            else if (diferenta_an < 1)
+                            {
+                                E = 10;
+                            }
+                            else if (diferenta_an < 3)
+                            {
+                                E = 30;
+                            }                            
+                            else if (diferenta_an < 5)
+                            {
+                                E = 40;
+                            }                            
+                            else if (diferenta_an < 10)
+                            {
+                                E = 60;
+                            }
+                            else
+                            {
+                                E = 80;
+                            }
+                            suma_plata = ( ( (em * B * 0.3) + (cmc_nr * D * 0.7) ) * (100 - E)/100 ) / 100;
+                            suma_plata = (double)System.Math.Round(suma_plata, 2);
+                            String text_final = "Total de plată (preț în euro): ";
+                            rezultat.Text += text_final += suma_plata;
+                        }
+                        catch(Exception)
+                        {
+                            MessageBox.Show("Nu ați scris anul fabricării!\r\nConsultați CIV-ul!", "Atenție!");
+                        }
+                    }
+                    catch(Exception)
+                    {
+                        MessageBox.Show("Nu ați selectat norma!\r\nConsultați CIV-ul!", "Atenție!");
+                    }
+                }
+                catch(Exception)
+                {
+                    MessageBox.Show("Nu ați introdus capacitatea cilindrică!\r\nConsultați CIV-ul! ", "Atenție!");
+                }
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Nu ați introdus emisiile! \r\n Consultați CIV-ul!", "Atenție!");
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void darkmode_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(46,46,46);
+            ForeColor = Color.FromArgb(247, 247, 247);
+
+            darkmode.BackColor = Color.FromArgb(46, 46, 46);
+            darkmode.ForeColor = Color.FromArgb(247, 247, 247);
+            
+            calculeaza.BackColor = Color.FromArgb(46, 46, 46);
+            calculeaza.ForeColor = Color.FromArgb(247, 247, 247);
+
+            emisie.BackColor = Color.FromArgb(46, 46, 46);
+            emisie.ForeColor = Color.FromArgb(247, 247, 247);
+
+            cmc.BackColor = Color.FromArgb(46, 46, 46);
+            cmc.ForeColor = Color.FromArgb(247, 247, 247);
+
+            norma.BackColor = Color.FromArgb(46, 46, 46);
+            norma.ForeColor = Color.FromArgb(247, 247, 247);
+
+            an.BackColor = Color.FromArgb(46, 46, 46);
+            an.ForeColor = Color.FromArgb(247, 247, 247);
+
+            rezultat.BackColor = Color.FromArgb(46, 46, 46);
+            rezultat.ForeColor = Color.FromArgb(247, 247, 247);
+
+            butonLight.BackColor = Color.FromArgb(46, 46, 46);
+            butonLight.ForeColor = Color.FromArgb(247, 247, 247);
+        }
+
+        private void butonLight_Click(object sender, EventArgs e)
+        {
+            BackColor = SystemColors.Control;
+            ForeColor = SystemColors.ControlText;
+
+            darkmode.BackColor = SystemColors.Control;
+            darkmode.ForeColor = SystemColors.ControlText;
+
+            calculeaza.BackColor = SystemColors.Control;
+            calculeaza.ForeColor = SystemColors.WindowText;
+
+            emisie.BackColor = SystemColors.Window;
+            emisie.ForeColor = SystemColors.WindowText;
+
+            cmc.BackColor = SystemColors.Window;
+            cmc.ForeColor = SystemColors.WindowText;
+
+            norma.BackColor = SystemColors.Window;
+            norma.ForeColor = SystemColors.WindowText;
+
+            an.BackColor = SystemColors.Window;
+            an.ForeColor = SystemColors.WindowText;
+
+            rezultat.BackColor = SystemColors.Control;
+            rezultat.ForeColor = SystemColors.WindowText;
+
+            butonLight.BackColor = SystemColors.Control;
+            butonLight.ForeColor = SystemColors.WindowText;
+        }
+    }
+}
